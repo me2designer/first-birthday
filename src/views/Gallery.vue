f83b04d539ee40e9931e9b4f7bd647eb6a24e6cc
designer <me2designer.com>
2022-10-08 01:51:50 +0900

    node 16.16.0 업그레이드

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,17 +3,17 @@
     <div class="inner">
       <h3 class="l-section__title">GALLERY</h3>
       <p class="l-section__description">귀여움 한도초과 하민이의 성장 스토리</p>
-      <GalleryList></GalleryList>
+      <!-- <GalleryList></GalleryList> -->
     </div>
   </section>
 </template>
 
 <script>
-import GalleryList from "./Gallery/GalleryList.vue";
+// import GalleryList from "./Gallery/GalleryList.vue";
 export default {
   name: "gallery",
   components: {
-    GalleryList,
+    // GalleryList,
   },
 };
 </script>
